9567b7ce207fdbbdca247f4813a5a84b719326dc
thebcrayon <106349257+thebcrayon@users.noreply.github.com>
2024-06-04 18:34:36 -0500

    Add getComputerChoice() to return str

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+    let choice;
+    if (randomNumber == 1) {
+        choice = 'Rock';
+    } else if (randomNumber == 2) {
+        choice = 'Paper';
+    } else {
+        choice = 'Scissors'
+    }
+    return choice;
+}
+
+console.log(getComputerChoice());
