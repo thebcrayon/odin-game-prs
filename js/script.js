0438d50a81fa009dcdb89ed0f794f986faad0302
thebcrayon <106349257+thebcrayon@users.noreply.github.com>
2024-06-05 00:19:05 -0500

    Add humanChoice and playRound functions, should have committed more.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,73 @@
 function getComputerChoice() {
-    let randomNumber = Math.floor(Math.random() * 3) + 1;
-    let choice;
-    if (randomNumber == 1) {
-        choice = 'Rock';
-    } else if (randomNumber == 2) {
-        choice = 'Paper';
-    } else {
-        choice = 'Scissors'
-    }
-    return choice;
+    let randomNumber = Math.floor(Math.random() * 3);
+    return selectionArray[randomNumber];
 }
 
-console.log(getComputerChoice());
+function getHumanChoice() {
+    let userChoice = prompt("Enter Choice \nType: 'Paper', 'Rock', or 'Scissors' \nOr hit 'Cancel' to exit. ");
+    let arrayToLowerCase = selectionArray.map(item => item.toLowerCase());
+    if (userChoice != undefined) {
+        if (arrayToLowerCase.includes(userChoice.toLowerCase())) {
+            return userChoice;
+        } else if (userChoice == '') {
+            alert('Field left blank.');
+            return getHumanChoice();
+        } else {
+            alert('Word not recognized. Check spelling and try again.');
+            return getHumanChoice();
+        }
+    }
+}
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
+    let winMsg = "You win: ";
+    let loseMsg = "You lose: ";
+    switch (humanChoice) {
+        case 'rock':
+            if (computerChoice == 'paper') {
+                alert(loseMsg + `${computerChoice} beats ${humanChoice}`);
+                computerScore++;
+            } else if (computerChoice == 'scissors') {
+                alert(winMsg + `${humanChoice} beats ${computerChoice}`);
+                humanScore++;
+            } else {
+                alert(`It's a tie! You selected ${humanChoice} and the computer selected ${computerChoice}`);
+            }
+            break;
+
+        case 'paper':
+            if (computerChoice == 'scissors') {
+                alert(loseMsg + `${computerChoice} beats ${humanChoice}`);
+                computerScore++;
+            } else if (computerChoice == 'rock') {
+                alert(winMsg + `${humanChoice} beats ${computerChoice}`);
+                humanScore++;
+            } else {
+                alert(`It's a tie! You selected ${humanChoice} and the computer selected ${computerChoice}`);
+            }
+            break;
+
+        case 'scissors':
+            if (computerChoice == 'rock') {
+                alert(loseMsg + `${computerChoice} beats ${humanChoice}`);
+                computerScore++;
+            } else if (computerChoice == 'paper') {
+                alert(winMsg + `${humanChoice} beats ${computerChoice}`);
+                humanScore++;
+            } else {
+                alert(`It's a tie! You selected ${humanChoice} and the computer selected ${computerChoice}`);
+            }
+            break;
+    }
+}
+
+const selectionArray = ['Rock', 'Paper', 'Scissors'];
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+let humanScore = 0;
+let computerScore = 0;
+playRound(humanChoice, computerChoice);
+
+
