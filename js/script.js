9fb577a5eb31f65ee772f55978fc3bcd42bb53b3
thebcrayon <106349257+thebcrayon@users.noreply.github.com>
2024-06-05 22:45:57 -0500

    Add playGame function outputs game stats

--- js/script.js
+++ js/script.js
@@ -28,46 +28,60 @@ function playRound(humanChoice, computerChoice) {
         case 'rock':
             if (computerChoice == 'paper') {
                 alert(loseMsg + `${computerChoice} beats ${humanChoice}`);
-                computerScore++;
+                return false;
             } else if (computerChoice == 'scissors') {
                 alert(winMsg + `${humanChoice} beats ${computerChoice}`);
-                humanScore++;
+                return true //humanScore++;
             } else {
                 alert(`It's a tie! You selected ${humanChoice} and the computer selected ${computerChoice}`);
+                return undefined;
             }
             break;
 
         case 'paper':
             if (computerChoice == 'scissors') {
                 alert(loseMsg + `${computerChoice} beats ${humanChoice}`);
-                computerScore++;
+                return false; 
             } else if (computerChoice == 'rock') {
                 alert(winMsg + `${humanChoice} beats ${computerChoice}`);
-                humanScore++;
+                return true;
             } else {
                 alert(`It's a tie! You selected ${humanChoice} and the computer selected ${computerChoice}`);
+                return undefined;
             }
             break;
 
         case 'scissors':
             if (computerChoice == 'rock') {
                 alert(loseMsg + `${computerChoice} beats ${humanChoice}`);
-                computerScore++;
+                return false;
             } else if (computerChoice == 'paper') {
                 alert(winMsg + `${humanChoice} beats ${computerChoice}`);
-                humanScore++;
+                return true;
             } else {
                 alert(`It's a tie! You selected ${humanChoice} and the computer selected ${computerChoice}`);
+                return undefined;
             }
             break;
     }
 }
 
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+    let isTie = 0;
+    for (i = 1; i <= 5; i++) {
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+        let getWinner = playRound(humanChoice, computerChoice);
+        if (getWinner != undefined) {
+            (getWinner) ? humanScore++ : computerScore++;
+        } else {
+            isTie++;
+        }
+    }
+    console.log(`Final Score: YOU : ${humanScore} | COMPUTER : ${computerScore} | TIES : ${isTie}`);
+}
+
 const selectionArray = ['Rock', 'Paper', 'Scissors'];
-const humanChoice = getHumanChoice();
-const computerChoice = getComputerChoice();
-let humanScore = 0;
-let computerScore = 0;
-playRound(humanChoice, computerChoice);
-
-
+playGame();
