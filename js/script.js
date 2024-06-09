bbb3a5bd22373bc0d8e2a2351f48c09bdae9eb4a
thebcrayon <106349257+thebcrayon@users.noreply.github.com>
2024-06-09 16:48:27 -0500

    Refactor main functions. Make ready for new logic

--- js/script.js
+++ js/script.js
@@ -1,87 +1,90 @@
+const optionList = document.querySelector('#options');
+const listOfChoices = document.querySelectorAll('#options>li');
+const selectionArray = [
+    listOfChoices.item(0).textContent,
+    listOfChoices.item(1).textContent,
+    listOfChoices.item(2).textContent
+]
+
+optionList.addEventListener('click', (event) => {
+    getHumanChoice(event.target.textContent);
+});
+
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
     return selectionArray[randomNumber];
 }
 
-function getHumanChoice() {
-    let userChoice = prompt("Enter Choice \nType: 'Paper', 'Rock', or 'Scissors' \nOr hit 'Cancel' to exit. ");
-    let arrayToLowerCase = selectionArray.map(item => item.toLowerCase());
-    if (userChoice != undefined) {
-        if (arrayToLowerCase.includes(userChoice.toLowerCase())) {
-            return userChoice;
-        } else if (userChoice == '') {
-            alert('Field left blank.');
-            return getHumanChoice();
-        } else {
-            alert('Word not recognized. Check spelling and try again.');
-            return getHumanChoice();
-        }
-    }
+function getHumanChoice(textContent) {
+    let humanChoice = textContent;
+    playRound(humanChoice, getComputerChoice());
 }
+
 
 function playRound(humanChoice, computerChoice) {
-    humanChoice = humanChoice.toLowerCase();
-    computerChoice = computerChoice.toLowerCase();
-    let winMsg = "You win: ";
-    let loseMsg = "You lose: ";
-    switch (humanChoice) {
-        case 'rock':
-            if (computerChoice == 'paper') {
-                alert(loseMsg + `${computerChoice} beats ${humanChoice}`);
-                return false;
-            } else if (computerChoice == 'scissors') {
-                alert(winMsg + `${humanChoice} beats ${computerChoice}`);
-                return true //humanScore++;
-            } else {
-                alert(`It's a tie! You selected ${humanChoice} and the computer selected ${computerChoice}`);
-                return undefined;
-            }
-            break;
+    alert(humanChoice + " : " + computerChoice);
+    //     humanChoice = humanChoice.toLowerCase();
+    //     computerChoice = computerChoice.toLowerCase();
+    //     let winMsg = "You win: ";
+    //     let loseMsg = "You lose: ";
+    //     switch (humanChoice) {
+    //         case 'rock':
+    //             if (computerChoice == 'paper') {
+    //                 alert(loseMsg + `${computerChoice} beats ${humanChoice}`);
+    //                 return false;
+    //             } else if (computerChoice == 'scissors') {
+    //                 alert(winMsg + `${humanChoice} beats ${computerChoice}`);
+    //                 return true //humanScore++;
+    //             } else {
+    //                 alert(`It's a tie! You selected ${humanChoice} and the computer selected ${computerChoice}`);
+    //                 return undefined;
+    //             }
+    //             break;
 
-        case 'paper':
-            if (computerChoice == 'scissors') {
-                alert(loseMsg + `${computerChoice} beats ${humanChoice}`);
-                return false; 
-            } else if (computerChoice == 'rock') {
-                alert(winMsg + `${humanChoice} beats ${computerChoice}`);
-                return true;
-            } else {
-                alert(`It's a tie! You selected ${humanChoice} and the computer selected ${computerChoice}`);
-                return undefined;
-            }
-            break;
+    //         case 'paper':
+    //             if (computerChoice == 'scissors') {
+    //                 alert(loseMsg + `${computerChoice} beats ${humanChoice}`);
+    //                 return false; 
+    //             } else if (computerChoice == 'rock') {
+    //                 alert(winMsg + `${humanChoice} beats ${computerChoice}`);
+    //                 return true;
+    //             } else {
+    //                 alert(`It's a tie! You selected ${humanChoice} and the computer selected ${computerChoice}`);
+    //                 return undefined;
+    //             }
+    //             break;
 
-        case 'scissors':
-            if (computerChoice == 'rock') {
-                alert(loseMsg + `${computerChoice} beats ${humanChoice}`);
-                return false;
-            } else if (computerChoice == 'paper') {
-                alert(winMsg + `${humanChoice} beats ${computerChoice}`);
-                return true;
-            } else {
-                alert(`It's a tie! You selected ${humanChoice} and the computer selected ${computerChoice}`);
-                return undefined;
-            }
-            break;
-    }
+    //         case 'scissors':
+    //             if (computerChoice == 'rock') {
+    //                 alert(loseMsg + `${computerChoice} beats ${humanChoice}`);
+    //                 return false;
+    //             } else if (computerChoice == 'paper') {
+    //                 alert(winMsg + `${humanChoice} beats ${computerChoice}`);
+    //                 return true;
+    //             } else {
+    //                 alert(`It's a tie! You selected ${humanChoice} and the computer selected ${computerChoice}`);
+    //                 return undefined;
+    //             }
+    //             break;
+    //     }
 }
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-    let isTie = 0;
-    for (i = 1; i <= 5; i++) {
-        let humanChoice = getHumanChoice();
-        let computerChoice = getComputerChoice();
-        let getWinner = playRound(humanChoice, computerChoice);
-        if (getWinner != undefined) {
-            (getWinner) ? humanScore++ : computerScore++;
-        } else {
-            isTie++;
-        }
-    }
-    console.log(`Final Score: YOU : ${humanScore} | COMPUTER : ${computerScore} | TIES : ${isTie}`);
-}
+// function playGame() {
+//     let humanScore = 0;
+//     let computerScore = 0;
+//     let isTie = 0;
+//     for (i = 1; i <= 5; i++) {
+//         let humanChoice = getHumanChoice();
+//         let computerChoice = getComputerChoice();
+//         let getWinner = playRound(humanChoice, computerChoice);
+//         if (getWinner != undefined) {
+//             (getWinner) ? humanScore++ : computerScore++;
+//         } else {
+//             isTie++;
+//         }
+//     }
+//     console.log(`Final Score: YOU : ${humanScore} | COMPUTER : ${computerScore} | TIES : ${isTie}`);
+// }
 
-const selectionArray = ['Rock', 'Paper', 'Scissors'];
-playGame();
+
+// //playGame();
